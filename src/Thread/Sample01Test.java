7641d0a6bfee05da953f794ba1e1086a459b19c1
KumaKuma0421 <kumakuma0421@gmail.com>
2018-10-01 22:31:54 +0900

    スレッドのサンプル

---
+++ src/Thread/Sample01Test.java
@@ -0,0 +1,36 @@
+package Thread;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Sample01Test extends Sample01 {
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testThread2() {
+        this.runThread2();
+    }
+
+    @Test
+    public void testThread4() {
+        this.runThread4();
+    }
+}
