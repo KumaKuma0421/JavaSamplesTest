d047e2ab0c30c30dd9c275e53793d357d56939f2
KumaKuma0421 <kumakuma0421@gmail.com>
2018-10-08 00:51:32 +0900

    ログ関係を追加

---
+++ src/Log4j2/Sample01Test.java
@@ -0,0 +1,37 @@
+package Log4j2;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Sample01Test extends Sample01 {
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+
+    @Test
+    void test01() {
+        this.Test01();
+    }
+
+    @Test
+    void test02() {
+        this.Test02();
+    }
+
+}
